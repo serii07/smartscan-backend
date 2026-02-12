05fae930cb2846d53ca4b726f0f7c7118e387b2f
Hrishikesh <biozerdpubg@gmail.com>
2026-02-13 03:56:07 +0530

    changes to blacklist

--- services.py
+++ services.py
@@ -10,7 +10,7 @@ def clean_product_name(raw_product_name: str) -> str:
     blacklist = [
         "buy", "online", "price", "at", "best", ":", "|",
         "amazon.in", "flipkart.com", "aap ka bazar", "beauty", "BigBasket",
-        "Aapkabazaar.co", "Amazon"
+        "Aapkabazar.co", "Amazon"
     ]
 
     words = cleaned.split()
